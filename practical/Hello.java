02148441a6c56034bfd35fdfd54b1023a9b6577a
Hema Nalawade <98449213+HemaNalawade@users.noreply.github.com>
2023-01-20 16:42:13 +0530

    Add files via upload

---
+++ practical/Hello.java
@@ -0,0 +1,16 @@
+class My{
+	public static void display()
+	{
+		System.out.println("Hii...");
+	}
+}
+
+class Hello{
+public static void main(String args[])
+{
+System.out.println("My First Program.....!");
+
+My.display();
+}
+
+}
