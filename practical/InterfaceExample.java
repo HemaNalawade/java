c8b171a88e5a0662c7d2241a9a38a5dd404b130a
Hema Nalawade <98449213+HemaNalawade@users.noreply.github.com>
2023-02-13 20:31:09 +0530

    Add files via upload

---
+++ practical/InterfaceExample.java
@@ -0,0 +1,43 @@
+interface Sports{//parent1
+    public void homeTeam(String name);
+	public void team(int noOfPlayers);
+}
+interface Event{//parent2
+   public void eventName(String ename);
+   public void eventDate(String edate);
+   public void eventPlace(String place);
+}
+class FootBall implements Sports{
+    public void homeTeam(String name)
+	{
+	name="Pune";
+	System.out.println("our team is representing" +name);
+	}
+	public void team(int noOfPlayers){
+	noOfPlayers=11;
+	System.out.println("Total no of players" +noOfPlayers);
+	}
+	public void goals(int score){
+	  System.out.println("Total no of score "+score);
+	}
+	public void eventName(String ename){
+	  System.out.println("Event company name "+ename);
+	}
+   public void eventDate(String edate){
+	   System.out.println("Event date"+edate);
+   }
+   public void eventPlace(String place){
+	   System.out.println("Event place "+place);
+   }
+}
+class InterfaceExample{
+  public static void main(String args[]){
+   FootBall b =new FootBall();
+   b.homeTeam("Maharastra");
+   b.team(11);
+   b.goal(2);
+   b.eventName("FIFA");
+   b.eventDate("1st mar 2023");
+   b.eventPlace("USA");
+  }
+}
